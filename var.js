f8be8634f5fd41012ae49cab6737f86bfda1150b
ggggggdb <92688941+ggggggdb@users.noreply.github.com>
2023-04-18 00:47:07 +0300

    Update var.js

--- var.js
+++ var.js
@@ -8,19 +8,19 @@ var bath = document.getElementById('bath');
            
 form1.onchange = form1.oninput = calcTotal1; 
 function calcTotal1() {	
-      var typeValue = Form1.selectionName.value;	
-      var bathValue = Form1.bathType.value;	
-      var ceilValue = Form1.selectionCeil.value;	
-      var repairValue = Form1.selectionRepair.value;
-      var repaiValue = Form1.selectionR.value;
-      var repaqValue = Form1.selectionv.value;
-      var repaaValue = Form1.selectionu.value;
-      var repazValue = Form1.selectiono.value;
-      var repapValue = Form1.selectionp.value;
-      var etagValue = Form1.etag1.value;
-      var balconValue = Form1.balcon1.value;
-      var windowValue = Form1.window1.value;
-      var transportValue = Form1.tq.value;
+      var typeValue = form1.selectionName.value;	
+      var bathValue = form1.bathType.value;	
+      var ceilValue = form1.selectionCeil.value;	
+      var repairValue = form1.selectionRepair.value;
+      var repaiValue = form1.selectionR.value;
+      var repaqValue = form1.selectionv.value;
+      var repaaValue = form1.selectionu.value;
+      var repazValue = form1.selectiono.value;
+      var repapValue = form1.selectionp.value;
+      var etagValue = form1.etag1.value;
+      var balconValue = form1.balcon1.value;
+      var windowValue = form1.window1.value;
+      var transportValue = form1.tq.value;
     
       
       
